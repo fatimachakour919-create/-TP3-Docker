2d370bb3300bc47404242bf4fa2f69f6cb3ccb8a
fatimazhra <fatimachakour919@gmail.com>
2026-03-16 11:43:46 +0100

    fich ajout

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,16 +1,11 @@
-import unittest
 from app.calculator import addition, multiplication
 
-class TestCalculator(unittest.TestCase):
-    def test_addition(self):
-        self.assertEqual(addition(2, 3), 5)
-        self.assertEqual(addition(-1, 1), 0)
-        self.assertEqual(addition(0, 0), 0)
+def test_addition():
+    assert addition(2, 3) == 5
+    assert addition(-1, 1) == 0
+    assert addition(0, 0) == 0
 
-    def test_multiplication(self):
-        self.assertEqual(multiplication(2, 3), 6)
-        self.assertEqual(multiplication(-1, 1), -1)
-        self.assertEqual(multiplication(0, 5), 0)
-
-if _name_ == '_main_':
-    unittest.main()
+def test_multiplication():
+    assert multiplication(2, 3) == 6
+    assert multiplication(-1, 1) == -1
+    assert multiplication(0, 5) == 0
